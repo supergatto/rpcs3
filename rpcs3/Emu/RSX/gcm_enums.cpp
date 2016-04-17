5cda4d59c940b75cd2b867e263e3e1af737fbf3d
Zangetsu38 <Dante38@free.fr>
2016-04-18 01:08:57 +0200

    Debug

--- rpcs3/Emu/RSX/gcm_enums.cpp
+++ rpcs3/Emu/RSX/gcm_enums.cpp
@@ -700,6 +700,7 @@ enum
 	CELL_GCM_DECR = 0x1E03,
 	CELL_GCM_INCR_WRAP = 0x8507,
 	CELL_GCM_DECR_WRAP = 0x8508,
+	CELL_GCM_INVERT = 0x150A,
 };
 
 rsx::stencil_op rsx::to_stencil_op(u16 in)
@@ -710,6 +711,7 @@ rsx::stencil_op rsx::to_stencil_op(u16 in)
 	case CELL_GCM_REPLACE: return rsx::stencil_op::replace;
 	case CELL_GCM_INCR: return rsx::stencil_op::incr;
 	case CELL_GCM_DECR: return rsx::stencil_op::decr;
+	case CELL_GCM_INVERT: return rsx::stencil_op::invert;
 	case CELL_GCM_INCR_WRAP: return rsx::stencil_op::incr_wrap;
 	case CELL_GCM_DECR_WRAP: return rsx::stencil_op::decr_wrap;
 	case CELL_GCM_ZERO: return rsx::stencil_op::zero;
@@ -797,7 +799,6 @@ enum
 	CELL_GCM_OR = 0x1507,
 	CELL_GCM_NOR = 0x1508,
 	CELL_GCM_EQUIV = 0x1509,
-	CELL_GCM_INVERT = 0x150A,
 	CELL_GCM_OR_REVERSE = 0x150B,
 	CELL_GCM_COPY_INVERTED = 0x150C,
 	CELL_GCM_OR_INVERTED = 0x150D,
@@ -992,6 +993,7 @@ rsx::blit_engine::context_surface rsx::blit_engine::to_context_surface(u32 in)
 
 enum
 {
+	CELL_GCM_CONTEXT_DMA_MEMORY_HOST_BUFFER = 0xFEED0001,
 	CELL_GCM_CONTEXT_DMA_TO_MEMORY_GET_REPORT = 0x66626660,
 	CELL_GCM_CONTEXT_DMA_REPORT_LOCATION_MAIN = 0xBAD68000,
 };
@@ -1000,6 +1002,7 @@ rsx::blit_engine::context_dma rsx::blit_engine::to_context_dma(u32 in)
 {
 	switch (in)
 	{
+	case CELL_GCM_CONTEXT_DMA_MEMORY_HOST_BUFFER: return rsx::blit_engine::context_dma::memory_host_buffer;
 	case CELL_GCM_CONTEXT_DMA_TO_MEMORY_GET_REPORT: return rsx::blit_engine::context_dma::to_memory_get_report;
 	case CELL_GCM_CONTEXT_DMA_REPORT_LOCATION_MAIN: return rsx::blit_engine::context_dma::report_location_main;
 	}
